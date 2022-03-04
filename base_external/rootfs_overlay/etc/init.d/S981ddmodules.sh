3441f44ddd09508a2fd8f7fcdc8331fbe1cdc2b3
shreyan-collab <shdh7090@colorado.edu>
2022-03-04 14:17:45 -0700

    Fix faulty error not working

--- base_external/rootfs_overlay/etc/init.d/S981ddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S981ddmodules.sh
@@ -4,15 +4,16 @@ case "$1" in
 	start)
 		echo "Loading module misc-modules and scull"
 		modprobe hello
-		/usr/bin/scull_load
 		/usr/bin/module_load faulty
+		/usr/bin/scull_load
 		
 		;;
 	stop)
 		echo "Stopping scull and misc-modules"
 		rmmod hello
-		/usr/bin/scull_unload
 		/usr/bin/module_unload faulty
+		/usr/bin/scull_unload
+		
 		
 		;;
 	*)
